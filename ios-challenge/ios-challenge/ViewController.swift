f28309ac99ad2ccf4fae098d0394820f42591a66
Justin Pradier <justin.pradier@integriti.com.au>
2016-06-09 23:16:22 +0000

    Initial Project Creation

---
+++ ios-challenge/ios-challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-challenge
+//
+//  Created by Integriti on 6/9/16.
+//  Copyright © 2016 Chromecide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
